1e1aa8ba3b6aabffd3081c8f8e41bc253d696bdb
felix.krug <felix.krug@vogel.de>
2021-06-07 10:54:11 +0200

    feat: Bot reacts now to PM from new User User now can with a predefined Message get a Custom Role. Further Changes will be made to make it more dynamic.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Reflection.Metadata.Ecma335;
 using DSharpPlus.Entities;
 using System.Collections.Generic;
+using MySqlX.XDevAPI;
+using Client = MySql.Data.MySqlClient.Memcached.Client;
+using DSharpPlus.EventArgs;
 
 
 namespace DiscordBot
@@ -74,20 +77,39 @@ namespace DiscordBot
         DiscordChannel[] arrayChannels;
         DiscordGuild[] arrayGuilds;
         DiscordRole[] arrayRoles;
+        GuildMemberAddEventArgs globalMember = null;
+        /*
+         * Event that reacts on User Join in your Guild.
+         */
         discord.GuildMemberAdded += async (s, e) =>
         {
-            arrayGuilds = new List<DiscordGuild>(discord.Guilds.Values).ToArray();
-            foreach (var guild in arrayGuilds)
+            await e.Member.CreateDmChannelAsync();
+            await e.Member.SendMessageAsync("Do you have any special Keycode?");
+            globalMember = e;
+        };
+        /*
+         * Event that reacts on Message written from User.
+         */
+        discord.MessageCreated += async (s, e) =>
+        {
+            /*
+             * Bot reacts to private User Message to get his defined Role.
+             */
+            if (e.Channel.IsPrivate == true && e.Message.Content.ToLower().StartsWith("givemerole"))
             {
-                arrayRoles = new List<DiscordRole>(guild.Roles.Values).ToArray();
-                
-                foreach (var role in arrayRoles)
+                arrayGuilds = new List<DiscordGuild>(discord.Guilds.Values).ToArray();
+                foreach (var guild in arrayGuilds)
                 {
-                    if (role.Name == "TestF")
-                        await e.Member.GrantRoleAsync(role);
-                }
-            } //await e.Member.GrantRoleAsync();
+                    arrayRoles = new List<DiscordRole>(guild.Roles.Values).ToArray();
 
+                    foreach (var role in arrayRoles)
+                    {
+                        if (role.Name == "TestF")
+
+                            await globalMember.Member.GrantRoleAsync(role);
+                    }
+                }
+            }
         };
         await discord.ConnectAsync();
         await Task.Delay(-1);
