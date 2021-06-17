2bef9854b9e25ed11225e8cc1131c4e46eae74a9
Tino Schmidt1 <T.Schmidt1@schueler.klara-oppenheimer-schule.de>
2021-06-17 14:17:16 +0200

    Methode erstellt

---
+++ STFU/MuteCommand.cs
@@ -0,0 +1,39 @@
+﻿using DiscordBot.Helper;
+using DiscordBot.DB;
+using DSharpPlus;
+using System.Threading.Tasks;
+using System;
+using DSharpPlus.EventArgs;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using DiscordBot.Rolemanager;
+using System.Collections.Specialized;
+using System.Threading.Channels;
+using Newtonsoft.Json.Linq;
+using DSharpPlus.Entities;
+using DSharpPlus.CommandsNext;
+namespace DiscordBot
+{
+  public class MuteCommand
+  {
+    //Prüfung der Berechtigung und Cooldown für den Befehl.
+    [Command("stfu"), RequireUserPermissions(Permissions.Administrator), Cooldown(1, 30, CooldownBucketType.Channel)]
+
+    public async Task STFU(CommandContext ctx)
+    {
+
+      //Was passieren sollte: Alle User in dem Sprachkanal des ausführenden Users, sollen gemuted werden.
+      //                      Ausgenommen davon ist der ausführende User.
+      //                      Nach ablauf eines weiteren Cooldowns werden die User wieder entmuted.
+      await ctx.RespondAsync("Silence!");
+    }
+
+
+    //Meldung falls die Prüfung fehlschlägt.
+    public ChecksFailedException(Command STFU, CommandContext ctx, IEnumerable<Permissions> failedChecks)
+    {
+      await ctx.RespondAsync("Access dinied!");
+    }
+  }
+}
