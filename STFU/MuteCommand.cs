f811692eb1a34388a11fc67870fd6d44b0c00a72
Felix1322 <felix.krug1302@gmx.de>
2021-07-22 17:18:32 +0200

    fix: STFU didnt work with more that 2 Persons
    The bot did something completely wrong as far as there were more
    than 2 Persons in the same Channel

--- STFU/MuteCommand.cs
+++ STFU/MuteCommand.cs
@@ -19,31 +19,44 @@ namespace DiscordBot.STFU
     /*
      * Mutes all other Users in your current Voice Channel.
      */
-    [Command("stfu"), RequireUserPermissions(Permissions.Administrator)]
+    [Command("stfu"), RequirePermissions(Permissions.Administrator), Cooldown(1, 5, CooldownBucketType.Channel)]
     public async Task STFU(CommandContext ctx)
     {
       MuteCommandTable mcTable = new MuteCommandTable();
+      string muted = String.Empty;
       mcTable.arrayChannels = new List<DiscordChannel>(ctx.Guild.Channels.Values).ToArray();
       foreach (var arrayChannel in mcTable.arrayChannels)
       {
-        mcTable.arrayMembers = new List<DiscordMember>(arrayChannel.Users).ToArray();
-        foreach (var arrayMember in mcTable.arrayMembers)
+        if (arrayChannel.Type == ChannelType.Voice)
         {
-          if (arrayMember.Id != ctx.Member.Id && arrayMember.IsMuted == false)
+          mcTable.arrayMembers = new List<DiscordMember>(arrayChannel.Users).ToArray();
+          foreach (var arrayMember in mcTable.arrayMembers)
           {
-            await arrayMember.SetMuteAsync(true);
-            await ctx.RespondAsync("Silence!");
-          }
-          else if (arrayMember.IsMuted)
-          {
-            await arrayMember.SetMuteAsync(false);
-            await ctx.RespondAsync("You now can speak again!!");
-          }
-          else
-          {
-            await arrayMember.SetMuteAsync(false);
+
+            if (arrayMember.Id != ctx.Member.Id && arrayMember.IsMuted == false)
+            {
+              await arrayMember.SetMuteAsync(true);
+              muted = "Usersmute";
+            }
+            else if (arrayMember.IsMuted)
+            {
+              await arrayMember.SetMuteAsync(false);
+              muted = "Usersunmute";
+            }
+            else if (arrayMember.Id == ctx.Member.Id)
+            {
+              await arrayMember.SetMuteAsync(false);
+            }
           }
         }
+      }
+      if (muted == "Usersmute")
+      {
+        await ctx.RespondAsync("Silence!");
+      }
+      else if (muted == "Usersunmute")
+      {
+        await ctx.RespondAsync("You can now speak again!");
       }
     }
   }
